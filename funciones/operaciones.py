6a7d01ae10dd9e86361784eaf79950574561c239
Aithana Cadena <9300489@alumnos.ufv.es>
2025-03-10 17:48:49 +0100

    creada rama y codigo en proceso de multiplicación

--- funciones/operaciones.py
+++ funciones/operaciones.py
@@ -17,3 +17,20 @@ def restar_polinomios(p1, p2):
         resultado[grado] = resultado.get(grado, 0) - coef
 
     return resultado
+
+## MULTIPLICACIÓN
+def multiplicar_polinomios(p1, p2):
+    resultado = {}
+
+    for grado1, coef1 in p1.items():
+        for grado2, coef2 in p2.items():
+            nuevo_grado = grado1 + grado2
+            nuevo_coef = coef1 * coef2
+
+            if nuevo_grado in resultado:
+                resultado[nuevo_grado] += nuevo_coef
+            else:
+                resultado[nuevo_grado] = nuevo_coef
+
+    return resultado
+
