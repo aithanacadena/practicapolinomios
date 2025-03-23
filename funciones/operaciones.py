3593480cb53982ffac2ad094a0e91cda636fe1a3
Aithana Cadena <9300489@alumnos.ufv.es>
2025-03-23 12:17:24 +0100

    comentarios en la operación de división

--- funciones/operaciones.py
+++ funciones/operaciones.py
@@ -43,6 +43,8 @@ def dividir_polinomios(dividendo, divisor):
     # Obtener el grado máximo de ambos polinomios
     grado_dividendo = max(dividendo.keys(), default=0)
     grado_divisor = max(divisor.keys(), default=0)
+        # default = 0, por si el dividendo es 0, que devuelva 0 como grado máximo
+
 
     # Si el grado del dividendo es menor que el del divisor, la división no es posible
     if grado_dividendo < grado_divisor:
@@ -52,19 +54,22 @@ def dividir_polinomios(dividendo, divisor):
 
     # Copia del dividendo para ir restando los múltiplos del divisor
     resto = dividendo.copy()
-
+# Dividimos mientras haya términos en el resto, y su grado sea al menos el del divisor
     while resto and max(resto.keys()) >= grado_divisor:
-        # Obtener el término líder del dividendo y del divisor
+        # Obtener el valor del resto que tiene asignado el máximo grado
         grado_resto = max(resto.keys())
         coef_resto = resto[grado_resto]
 
-        grado_cociente = grado_resto - grado_divisor
-        coef_cociente = coef_resto / divisor[grado_divisor]
+        # Dividimos el término con mayor grado del resto entre el término con mayor grado del divisor
+        grado_cociente = grado_resto - grado_divisor # Se restan los exponentes
+        coef_cociente = coef_resto / divisor[grado_divisor] # Se dividen los coeficientes
 
-        # Agregar el término al cociente
+        # Agregar el término que acabamos de obtener al cociente
         cociente[grado_cociente] = coef_cociente
 
         # Crear el polinomio a restar
+        # Construye un nuevo polinomio que será restado del "resto" en la división de polinomios
+        # Multiplicando el el divisor por el término recién encontrado
         polinomio_a_restar = {
             grado + grado_cociente: coef * coef_cociente
             for grado, coef in divisor.items()
@@ -73,11 +78,11 @@ def dividir_polinomios(dividendo, divisor):
         # Restar el polinomio obtenido del resto
         for grado, coef in polinomio_a_restar.items():
             if grado in resto:
-                resto[grado] -= coef
+                resto[grado] -= coef # Si el término está en el resto, lo restamos
                 if abs(resto[grado]) < 1e-10:  # Para evitar problemas con flotantes
-                    del resto[grado]
+                    del resto[grado] # Si se vuelve cero, lo eliminamos
             else:
-                resto[grado] = -coef
+                resto[grado] = -coef # Si no está, lo agregamos con el signo opuesto
 
     return (cociente, resto)
 
